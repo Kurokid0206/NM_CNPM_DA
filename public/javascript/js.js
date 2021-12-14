aa18b0690da24176e64a52851ba4ceda5d38f49a
Kurokid0206 <hungtyvuive@gmail.com>
2021-12-14 15:19:07 +0700

    new version

---
+++ public/javascript/js.js
@@ -0,0 +1,68 @@
+function show_find_course(){
+    var div = document.querySelector("#form-insert-course")
+    div.style.display = "none";
+    div = document.querySelector("#find-courses")
+    div.style.display = "flex";
+
+}
+
+function show_insert_course() {
+    var div = document.querySelector("#form-insert-course")
+    div.style.display = "flex";
+    div = document.querySelector("#find-courses")
+    div.style.display = "none";
+
+}
+
+function find_courses(){
+    var input=document.getElementById("find-courses-input")
+    console.log(input.value)
+    
+
+    var xhtml = new XMLHttpRequest();
+    xhtml.onload = function() {
+
+        input.value="";
+        var data=JSON.parse(this.responseText)
+        console.log(data)
+        document.getElementById("search-result").innerHTML=""
+        document.getElementById("search-result").append(grid_render(data))
+        //document.getElementById("search-result").innerHTML=htmlTable(data)
+    }
+
+    xhtml.open("POST", "find-courses");
+    xhtml.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhtml.send('keyword='+input.value);
+    return false;
+}
+
+function grid_render(data){
+    var new_grid=document.createElement('div')
+    new_grid.className="grid-container"
+    //console.log(headings)
+    data.forEach(element => {
+        var item = document.createElement('div');
+        item.className="grid-item"
+        Object.keys(element).forEach(heading=>{
+            var data=document.createElement('div')
+            data.innerHTML=element[heading]
+            item.append(data)
+        })
+        new_grid.append(item)
+    })
+
+    return new_grid
+}
+
+
+
+const row = html => `<tr>\n${html}</tr>\n`,
+      heading = object => row(Object.keys(object).reduce((html, heading) => (html + `<th>${heading}</th>`), '')),
+      datarow = object => row(Object.values(object).reduce((html, value) => (html + `<td>${value}</td>`), ''));
+//convert JSON to table                               
+function htmlTable(dataList) {
+  return `<table class="tbl">
+            ${heading(dataList[0])}
+            ${dataList.reduce((html, object) => (html + datarow(object)), '')}
+          </table>`
+}
--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-//P1
 // use node express
 const express = require("express");
 const app = express();
@@ -8,14 +7,73 @@ app.use(express.urlencoded({ extended: false }))
 // parse application/json
 app.use(express.json())
 
-const sql = require('mssql')
+//app.set("view engine","ejs");
+
+const sql = require('mssql');
+//const { config } = require("nodemon");
+
 
 app.listen(3000, function(){
 	console.log("server is listen on port 3000.");
 });
 
+// async function getData(qry) {
+// 	try {
+// 		let pool = await sql.connect(config);
+// 		let result = await pool.request().query(qry);
+// 		await pool.close();
+// 		return JSON.stringify(result)
+// 	} catch (error) {
+// 		console.log(error.message);
+// 		return error.message
+// 	}
+// };
+
+
+config = {
+    user: 'sa',
+    password: '.',
+    server: 'localhost',
+    database: 'qlKH',
+    port: 1433,
+    options: {
+        trustServerCertificate: true // change to true for local dev / self-signed certs
+    }
+}
+var result=""
+var qry=""
 //get home page
 app.get("/", function (req, res) {
-	res.sendFile(__dirname+"/index.html")
+	res.sendFile(__dirname+'/index.html');
 
+});
+
+app.post("/insert-course",function(req,res){
+	res.redirect("/");
+	
+	console.log(req.body);
+});
+
+
+
+app.post("/find-courses",function(req,res){
+	//tạo promise chắc chắn trả về
+	Promise.resolve('success').then(
+		async function () {
+			try {
+				let pool = await sql.connect(config);
+				let result = await pool.request()
+					.input('keyword', sql.NVarChar(50), `${req.body.keyword}`)
+					//.output('output_parameter', sql.VarChar(50))
+					.execute('sp_findCourses')
+				pool.close()
+				res.send(result.recordset)
+				console.log(result)
+				return 
+			} catch (error) {
+				console.log(error.message);
+				return error.message
+			}
+		}
+	)
 });
