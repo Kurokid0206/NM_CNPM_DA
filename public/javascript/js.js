3898a4f1be3c8c69a74fc26ab4a01566bdd52752
Yami91001 <67729979+Yami91001@users.noreply.github.com>
2021-12-21 20:52:54 +0700

    updated

--- public/javascript/js.js
+++ public/javascript/js.js
@@ -1,93 +1,16 @@
-function show_find_course(){
-    var div = document.querySelector("#form-insert-course")
-    div.style.display = "none";
-    div = document.querySelector("#find-courses")
-    div.style.display = "flex";
-    div = document.querySelector("#schedule")
-    div.style.display = "none";
-    div = document.querySelector("#form-change-info")
-    div.style.display = "none";
-    div = document.querySelector("#form-become-teacher")
-    div.style.display = "none";
-    div = document.querySelector("#form-update-cert")
-    div.style.display = "none";
-}
-
-function show_insert_course() {
-    var div = document.querySelector("#form-insert-course")
-    div.style.display = "flex";
-    div = document.querySelector("#find-courses")
-    div.style.display = "none";
-    div = document.querySelector("#schedule")
-    div.style.display = "none";
-    div = document.querySelector("#form-change-info")
-    div.style.display = "none";
-    div = document.querySelector("#form-become-teacher")
-    div.style.display = "none";
-    div = document.querySelector("#form-update-cert")
-    div.style.display = "none";
-}
+var ids=["form-insert-course","find-courses","my-courses"]
 
 
-function show_schedule() {
-    var div = document.querySelector("#schedule")
-    div.style.display = "flex";
-    div = document.querySelector("#form-insert-course")
-    div.style.display = "none";
-    div = document.querySelector("#find-courses")
-    div.style.display = "none";
-    div = document.querySelector("#form-change-info")
-    div.style.display = "none";
-    div = document.querySelector("#form-become-teacher")
-    div.style.display = "none";
-    div = document.querySelector("#form-update-cert")
-    div.style.display = "none";
+function show(id){
+    
+    ids.forEach(id=>{
+        var div = document.querySelector("#"+id);
+        div.style.display = "none";
+    })
+    var div = document.querySelector("#"+id);
+        div.style.display = "flex";
 }
 
-function change_info() {
-    var div = document.querySelector("#schedule")
-    div.style.display = "none";
-    div = document.querySelector("#form-insert-course")
-    div.style.display = "none";
-    div = document.querySelector("#find-courses")
-    div.style.display = "none";
-    div = document.querySelector("#form-change-info")
-    div.style.display = "flex";
-    div = document.querySelector("#form-become-teacher")
-    div.style.display = "none";
-    div = document.querySelector("#form-update-cert")
-    div.style.display = "none";
-}
-
-function apply_teacher() {
-    var div = document.querySelector("#schedule")
-    div.style.display = "none";
-    div = document.querySelector("#form-insert-course")
-    div.style.display = "none";
-    div = document.querySelector("#find-courses")
-    div.style.display = "none";
-    div = document.querySelector("#form-change-info")
-    div.style.display = "none";
-    div = document.querySelector("#form-become-teacher")
-    div.style.display = "flex";
-    div = document.querySelector("#form-update-cert")
-    div.style.display = "none";
-}
-
-function update_more_cert(){
-    var div = document.querySelector("#schedule")
-    div.style.display = "none";
-    div = document.querySelector("#form-insert-course")
-    div.style.display = "none";
-    div = document.querySelector("#find-courses")
-    div.style.display = "none";
-    div = document.querySelector("#form-change-info")
-    div.style.display = "none";
-    div = document.querySelector("#form-become-teacher")
-    div.style.display = "none";
-    div = document.querySelector("#form-update-cert")
-    div.style.display = "flex";
-}
 
 function find_courses(){
     var input=document.getElementById("find-courses-input")
@@ -128,29 +51,12 @@ function grid_render(data){
     return new_grid
 }
 
-const row = html => `<tr>\n${html}</tr>\n`,
-      heading = object => row(Object.keys(object).reduce((html, heading) => (html + `<th>${heading}</th>`), '')),
-      datarow = object => row(Object.values(object).reduce((html, value) => (html + `<td>${value}</td>`), ''));
-//convert JSON to table                               
-function htmlTable(dataList) {
-  return `<table class="tbl">
-            ${heading(dataList[0])}
-            ${dataList.reduce((html, object) => (html + datarow(object)), '')}
-          </table>`
-}
-
 function insert_course(){
     var course_name=document.getElementById("course_name");
     var course_grade=document.getElementById("course_grade");
     var course_class_num=document.getElementById("course_class_num");
     var course_min_student=document.getElementById("course_min_student");
     var fee=document.getElementById("fee");
-
-    // course_name=""
-    // course_grade=0
-    // course_class_num=0
-    // course_min_student=0
-    // fee=0
 
     var xhtml = new XMLHttpRequest();
     xhtml.onload = function() {
@@ -177,49 +83,19 @@ function insert_course(){
 }
 
 
+
 function show_my_courses(){
+    show('my-courses')
+    
     var xhtml = new XMLHttpRequest();
     xhtml.onload = function() {
         var data=JSON.parse(this.responseText)
         document.getElementById("my-courses").innerHTML=""
-        document.getElementById("my-courses").append(grid_render(data))
+        document.getElementById("my-courses").append(grid_render(data))  
     }
 
     xhtml.open("GET", "my-courses");
     xhtml.send();
 
     return false;
-<<<<<<< Updated upstream
-=======
 }
->>>>>>> Stashed changes
-
-function checkEmail() { 
-    var email = document.getElementById('email'); 
-    var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/; 
-    if (!filter.test(email.value)) { 
-        alert('Vui long nhap dia chi email hop le');
-        email.focus; 
-        return false; 
-    }
-    else{ 
-        return true;
-    } 
-} 
-
-function checkSDT() {
-    var SDT = document.getElementById('SDT');
-    var filter = /^[0]+[0-9]{9}/;
-    if (!filter.test(SDT.value)) {
-        alert('Vui long nhap so dien thoai hop le');
-        SDT.focus;
-        return false;
-    }
-    else {
-        return true;
-    }
-<<<<<<< Updated upstream
-}}
-=======
-}
->>>>>>> Stashed changes
